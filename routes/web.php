6f5b3f709509781f0751a7d4d0098c462f27891c
Ildebrando Quinchoa <ildebrandoefrain@gmail.com>
2023-12-06 09:01:10 -0400

    test message for route:cambiarRol

--- routes/web.php
+++ routes/web.php
@@ -64,21 +64,26 @@ Route::middleware(['auth','role:administrador'])->group(function(){
         $user = User::find($id);
         $users = App\Models\User::with('roles', 'permissions')->get();
 
-        if ($post['form-name'] == 'rol'){
-            if($post['accion'] == 'Agregar'){
-                $user->assignRole($post['rol']);
-            }elseif($post['accion'] == 'Eliminar'){
-                $user->removeRole($post['rol']);
-            }
-        }else{
-            if($post['accion'] == 'Agregar'){
-                $user->givePermissionTo($post['permission']);
-            }elseif($post['accion'] == 'Eliminar'){
-                $user->revokePermissionTo($post['permission']);
-            }
-        }
+        $uno =$post['form-name']=='rol';
+        $dos = $post['accion']=='Agregar';
 
-        return view('admin.listuser')->with('users', $users);
+        echo $post['form-name'] . $post['accion'] . $uno . $dos;
+
+        // if ($post['form-name'] == 'rol'){
+        //     if($post['accion'] == 'Agregar'){
+        //         $user->assignRole($post['rol']);
+        //     }elseif($post['accion'] == 'Eliminar'){
+        //         $user->removeRole($post['rol']);
+        //     }
+        // }else{
+        //     if($post['accion'] == 'Agregar'){
+        //         $user->givePermissionTo($post['permission']);
+        //     }elseif($post['accion'] == 'Eliminar'){
+        //         $user->revokePermissionTo($post['permission']);
+        //     }
+        // }
+
+        // return view('admin.listuser')->with('users', $users);
     })->name('admin.changeuserroleorpermission');
 });
 
